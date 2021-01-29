0f76c09c9c169cad72c371f9fea4f0bd3d1e0ee5
MayraT <mayratorresas@unimagdalena.edu.co>
2021-01-28 20:51:33 -0500

    Probando serial

--- acelerometro_periferico_mma8451/source/acelerometro_periferico_mma8451.c
+++ acelerometro_periferico_mma8451/source/acelerometro_periferico_mma8451.c
@@ -7,35 +7,61 @@
 #include "fsl_debug_console.h"
 
 #include "sdk_hal_gpio.h"
+#include "sdk_hal_uart0.h"
 
 int main(void) {
+	/* Init board hardware. */
+	BOARD_InitBootPins();
+	BOARD_InitBootClocks();
+	BOARD_InitBootPeripherals();
+	#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
+		/* Init FSL debug console. */
+		BOARD_InitDebugConsole();
+	#endif
+	(void)uart0Init(115200);
+	PRINTF("Usar teclado para controlar LEDs \r\n");
 
-	status_t resultado;
-
-
-  	/* Init board hardware. */
-    BOARD_InitBootPins();
-    BOARD_InitBootClocks();
-    BOARD_InitBootPeripherals();
-#ifndef BOARD_INIT_DEBUG_CONSOLE_PERIPHERAL
-    /* Init FSL debug console. */
-    BOARD_InitDebugConsole();
-#endif
-
-    PRINTF("Hello World\n");
-
-
-     resultado= gpioPutToggle(KPTB7);
+	PRINTF("r-R led ROJO \r\n ");
+	PRINTF("v-V LED verde\r\n");
+	PRINTF("a-A LED azul\r\n");
 
 
-    /* Force the counter to be placed into memory. */
-    volatile static int i = 0 ;
-    /* Enter an infinite loop, just incrementing a counter. */
-    while(1) {
-        i++ ;
-        /* 'Dummy' NOP to allow source level single stepping of
-            tight while() loop */
-        __asm volatile ("nop");
-    }
-    return 0 ;
+	//resultado = gpioPutToggle(KPTB7);
+	status_t status;
+	uint8_t letra;
+	/* Force the counter to be placed into memory. */
+	/* Enter an infinite loop, just incrementing a counter. */
+	while (1) {
+
+		if (uart0Ready()>0){
+			status=uart0Read(&letra);
+			printf("dato entrante: %c \r\n", letra );
+
+			switch (letra){
+
+			case'a':
+			case'A':
+				gpioPutToggle(KPTB10);
+				break;
+			case'r':
+				gpioPutValue(KPTB6,0);
+				break;
+			case'R':
+				gpioPutValue(KPTB6,1);
+				break;
+			case'v':
+				gpioPutHigh(KPTB7);
+				break;
+			case'V':
+				gpioPutLow(KPTB7);
+				break;
+
+			default:
+				printf("letra incorrecta :( \r\n");
+				break;
+			}
+
+		}
+	}
+	return 0;
 }
